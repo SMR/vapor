8c26441dbc3cadaaba41aec8bf7769d0c3142523
Samarjeet dubey <sdubey@aperotechnologies.com>
2016-11-06 20:34:14 +0530

    index page Added

--- HelloWorld/Sources/App/main.swift
+++ HelloWorld/Sources/App/main.swift
@@ -2,10 +2,8 @@ import Vapor
 
 let drop = Droplet()
 
-drop.get { req in
-    return try drop.view.make("welcome", [
-    	"message": drop.localization[req.lang, "welcome", "title"]
-    ])
+drop.get("html") { request in
+    return try drop.view.make("index.html")
 }
 
 drop.resource("posts", PostController())
